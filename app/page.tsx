f131881b695cbdd5ee17b9175e92d9a1045933da
Sidharth <sidharthinfernal@gmail.com>
2024-11-20 23:26:15 +0530

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
-import { auth } from "./lib/auth";
+import Link from "next/link";
+import { ArrowRight,Github } from "lucide-react";
 
 export default async function Home() {
-  const session = await auth();
+ 
   return (
-    <div>
-      <img 
-      src={session?.user?.image}
-      alt="name"
-      />
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl">RealTime Music Using Spotify Selection,Collaboration  </h1>
+      <div className="flex justify-center items-center space-x-5">
+        <Link
+        className="flex mt-5"
+        href={"/login"}
+        >Login with spotify
+        <ArrowRight/>
+        </Link>
+        <Link
+        className="p-2 border-2 bg-neutral-800 rounded-lg mt-3"
+        href={"https://github.com/sidhuiwnl/musicshare"}
+        >
+          <Github/>
+        </Link>
+      </div>
     </div>
   );
 }
