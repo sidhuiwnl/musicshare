4ab7f440b2ddfe81b8d01a9cc0d280659db63ab5
Sidharth <sidharthinfernal@gmail.com>
2024-11-20 22:10:36 +0530

    initialize and add authjs

---
+++ app/lib/auth.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import Spotify from "next-auth/providers/spotify";
+
+export const  { handlers, signIn, signOut, auth } = NextAuth({
+    providers : [Spotify],
+    callbacks  : {
+        async redirect(){
+            return "/"
+        }
+    }
+    
+})
---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+import { signIn } from "../lib/auth"
+
+export default function Login(){
+    return(
+        <div>
+            <form action={async() =>{
+                "use server"
+                await signIn("spotify")
+            }}>
+                <button type="submit">Signin with Spotify</button>
+            </form>
+        </div>
+    )
+}
